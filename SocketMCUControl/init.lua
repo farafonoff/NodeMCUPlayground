6361aca5acf9e8b5f160d6c8fc8ba6c81168b5be
Artem Farafonov <artem_farafonov@epam.com>
2017-10-18 11:40:53 +0400

    fixed restart in ap mode

--- SocketMCUControl/init.lua
+++ SocketMCUControl/init.lua
@@ -1,45 +1,4 @@
-print('HEAP:',node.heap())
-ap_ssid,ap_pass = "esp8266","optanex14";
-
-if (file.open('wificonf') == true)then
-   ssid = string.gsub(file.readline(), "\n", "");
-   pass = string.gsub(file.readline(), "\n", "");
-   file.close();
-end
-
-wifi.setmode(wifi.STATION)
---wifi.sta.config {ssid=ssid,pwd=pass}
---wifi.sta.autoconnect(1);
-known_fi = {}
-known_fi["netis_24"]="optanex14"
-selected_config = nil
-
-wifi.sta.getap(1, function(t)
-    print("\n\t\t\tSSID\t\t\t\t\tBSSID\t\t\t  RSSI\t\tAUTHMODE\t\tCHANNEL")
-    for bssid,v in pairs(t) do
-        local ssid, rssi, authmode, channel = string.match(v, "([^,]+),([^,]+),([^,]+),([^,]*)")
-        print(string.format("%32s",ssid).."\t"..bssid.."\t  "..rssi.."\t\t"..authmode.."\t\t\t"..channel)
-        if known_fi[ssid] then
-            selected_config = {ssid=ssid,pwd=known_fi[ssid]}
-        end
-    end
-    if selected_config then
-        wifi.sta.autoconnect(1);
-        wifi.sta.config(selected_config)
-        run()
-    else
-        wifi.setmode(wifi.SOFTAP)
-        wifi.ap.config{ssid=ap_ssid, pwd=ap_pass}
-        run()
-    end
-end)
-
---uart.setup(1, 9600, 8, uart.PARITY_NONE, uart.STOPBITS_1, 0)
---uart.write(1, "Hello world")
---print('IP:',wifi.sta.getip());
---print('MAC:',wifi.sta.getmac());
-
-function run()
+function car_run()
     state=""
     
     tmr.alarm(0,10, tmr.ALARM_AUTO, function() 
@@ -85,3 +44,49 @@ function run()
     port, ip = udpSocket:getaddr()
     print(string.format("local UDP socket address / port: %s:%d", ip, port))
 end
+
+print('HEAP:',node.heap())
+ap_ssid,ap_pass = "esp8266","optanex14";
+
+if (file.open('wificonf') == true)then
+   ssid = string.gsub(file.readline(), "\n", "");
+   pass = string.gsub(file.readline(), "\n", "");
+   file.close();
+end
+
+wifi.setmode(wifi.STATION)
+--wifi.sta.config {ssid=ssid,pwd=pass}
+--wifi.sta.autoconnect(1);
+known_fi = {}
+known_fi["netis_24"]="optanex14"
+selected_config = nil
+
+function run_softap()
+        print("hotspot not found, running softap")
+        wifi.setmode(wifi.SOFTAP)
+        wifi.ap.config{ssid=ap_ssid, pwd=ap_pass}
+        tmr.alarm(0,200,0,car_run)
+end
+
+wifi.sta.getap(1, function(t)
+    print("\n\t\t\tSSID\t\t\t\t\tBSSID\t\t\t  RSSI\t\tAUTHMODE\t\tCHANNEL")
+    for bssid,v in pairs(t) do
+        local ssid, rssi, authmode, channel = string.match(v, "([^,]+),([^,]+),([^,]+),([^,]*)")
+        print(string.format("%32s",ssid).."\t"..bssid.."\t  "..rssi.."\t\t"..authmode.."\t\t\t"..channel)
+        if known_fi[ssid] then
+            selected_config = {ssid=ssid,pwd=known_fi[ssid]}
+        end
+    end
+    if selected_config then
+        wifi.sta.autoconnect(1);
+        wifi.sta.config(selected_config)
+        tmr.alarm(0,500,0,run)
+    else
+        tmr.alarm(0,200,0,run_softap)
+    end
+end)
+
+--uart.setup(1, 9600, 8, uart.PARITY_NONE, uart.STOPBITS_1, 0)
+--uart.write(1, "Hello world")
+--print('IP:',wifi.sta.getip());
+--print('MAC:',wifi.sta.getmac());
